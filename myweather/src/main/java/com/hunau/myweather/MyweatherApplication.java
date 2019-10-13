36b40b4dded828272fd05956ad18a4fe61c851b7
Rpurgatory <1943291256@qq.com>
2019-10-13 19:43:16 +0800

    我的第一个天气网页

---
+++ myweather/src/main/java/com/hunau/myweather/MyweatherApplication.java
@@ -0,0 +1,14 @@
+package com.hunau.myweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class MyweatherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyweatherApplication.class, args);
+    }
+
+}
